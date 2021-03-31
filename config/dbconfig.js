25be3fb6248656cc0ef3445ccdc44c8ae24eb166
Cre8ive-collins <wilsoncollins44@yahoo.com>
2021-03-31 12:04:56 +0100

    ...

---
+++ config/dbconfig.js
@@ -0,0 +1,13 @@
+const sql = require('mysql')
+require('dotenv').config()
+
+
+const pool = sql.createPool({
+    connectionLimit: 10,
+    host: process.env.HOST,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+})
+
+module.exports = pool
--- index.js
+++ index.js
@@ -2,63 +2,37 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const sql = require('mysql');
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport')
+require('dotenv').config()
 
 
 const pool = sql.createPool({
     connectionLimit: 10,
-    host: "localhost",
-    user: "collins",
-    password: "collinsP",
-    database: "hilton",
+    host: process.env.HOST,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
 })
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+
+
+app.use(express.static(path.join(__dirname, 'assets')))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-async function main(params) {
-    // let testAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport(smtpTransport({
-        host: "webmail.softnoonng.com",
-        tls:{
-            rejectUnauthorized: false
-        },
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'collinswilson@softnoonng.com',
-            pass: 'Newaccess@21'
-        },
-    }));
 
-    let info = await transporter.sendMail({
-        from: '"Collins Wilson (Node App)" <collinswilson@softnoonng.com>',
-        to: 'collinswilson44@gmail.com',
-        text: `
-            New Application (LEAD)
-            Name : ${params.name},
-            email: ${params.email},
-            phone: ${params.phone}
-        `
-    });
-
-    console.log('message sent')
-}
 
 app.get('/' , (req, res) => {
-    // res.send('hello')
 
     pool.getConnection((err,con) => {
         if (err) throw err;
         // console.log(con)
         con.query('SELECT * from leads', (err, result) => {
             con.release()
-
             if(!err){
                 // res.send(result)
                 res.render('index')
@@ -69,23 +43,11 @@ app.get('/' , (req, res) => {
     })
 })
 
-app.post('/landingform', (req, res) => {
-    const params = req.body
 
-    pool.getConnection((err, con) => {
-        if (err) throw err;
 
-        con.query('INSERT INTO leads SET ?', params, (err, result) => {
-            con.release()
-            if(!err){
-                main(params)
-                res.send(`user ${params.name} added to DB Successfully`)
-            }else{
-                console.log(err)
-            }
-        })
-    })
-})
+const landingForm = require('./routes/landingform');
+app.use('/landingform', landingForm);
+
 
 app.listen(3000, function(){
     console.log('app running on port 3000')
