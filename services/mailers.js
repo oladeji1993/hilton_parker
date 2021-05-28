67d966b75f2e58e60fb278f6136b46685bde609a
oladeji1993 <olanipekunoladeji282@yahoo.com>
2021-05-28 07:58:20 +0100

    recent

--- services/mailers.js
+++ services/mailers.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 
 // ADMIN MAILER
-async function newLead(params) {
+async function newLead(params, admin) {
     let transporter = nodemailer.createTransport(smtpTransport({
         host: "webmail.softnoonng.com",
         tls:{
@@ -21,7 +21,7 @@ async function newLead(params) {
 
     let info = await transporter.sendMail({
         from: '"HILLPAD SERVICES" <collinswilson@softnoonng.com>',
-        to: 'collinswilson@softnoonng.com',
+        to: `${admin.email}`,
         subject:`Application Mail`,
         html: `
                         
@@ -1011,6 +1011,102 @@ async function notify(lead) {
 
 }
 
+async function contactus(msg) {
+    let transporter = nodemailer.createTransport(smtpTransport({
+        host: "webmail.softnoonng.com",
+        tls:{
+            rejectUnauthorized: false
+        },
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'collinswilson@softnoonng.com',
+            pass: process.env.MAIL_PASSWORD 
+        },
+    }));
 
 
-module.exports = {newLead, notify, applied, forgot_password, contact, document_upload, agent, agentofficer, agent_reset_password,verified};
+    let info = await transporter.sendMail({
+        from: '"HILLPAD SERVICES" <collinswilson@softnoonng.com>',
+        to: 'collinswilson@softnoonng.com', 
+        subject: 'Contact us form',
+        html: `
+                        
+        <!doctype html>
+        <html lang="en-US">
+
+        <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Contact us Mail</title>
+        <meta name="description" content="Contact us Mail">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+        </head>
+
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Contact us Mail</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;"><br>
+                                                This is a contact us message.<br>
+                                                Find the contact information below:<br>
+
+                                                NAME : ${msg.name},<br>
+                                                EMAIL: ${msg.email},<br>
+                                                MESSAGE: ${msg.message}<br>
+
+                                                Kindly act upon
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+        </body>
+
+        </html>
+        `
+    });
+
+}
+
+
+
+
+module.exports = {newLead, contactus, notify, applied, forgot_password, contact, document_upload, agent, agentofficer, agent_reset_password,verified};
